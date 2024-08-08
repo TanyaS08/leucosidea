e52cb0b50b0e0f44ceee799a341c9a01d71d6f8b
Tanya Strydom <tanya.strydom@icloud.com>
2024-08-08 10:37:54 +0100

    🤨 community PCA but it looks sus...

--- scripts/03_pca.R
+++ scripts/03_pca.R
@@ -8,22 +8,103 @@ library(ade4)
 library(ggrepel)
 
 ####Import data####
+# array for species community
+COMMarray <- read.table("data/spp_richness.txt", header=T, row.names = 1) %>% 
+  select(-Site) %>% 
+  # get microsite from ID
+  mutate(Habitat = str_extract(row.names(.), ".{1}$")) %>%
+  # standardise naming
+  mutate(Habitat = case_when(Habitat == "U" ~ "Under",
+                             TRUE ~ "Away")) %>%
+  # reorder
+  select(Habitat, everything())
+#remove NA
+COMMarray <- na.omit(COMMarray)
 
-FTarray = read.table("data/FT.txt",header=T, row.names = 1) %>% 
+# array for traits
+FTarray = read.table("data/FT.txt", header = T, row.names = 1) %>% 
   select(Plot, Species, Habitat, Chlorophyll, Toughness, PHeight, SLA, LDMC) %>%
   mutate(Habitat = case_when(Habitat == "Under" ~ "Under",
-                             TRUE ~ "Away"))
+                             TRUE ~ "Away"))    
 #remove NA
 FTarray <- na.omit(FTarray)
-
-dat = FTarray[4:ncol(FTarray)]
-Blob_sp = FTarray$Species
-Blob_microsite = FTarray$Habitat
 
 titre <- c(Chlorophyll = "Chlorophyll~(mg/m^2)", PHeight = "Plant~height~(m)", SLA = "SLA~(cm^2%.%g)",
            LDMC = "LDMC", Toughness = "Toughness~(N)")[colnames(dat)]
 
-####Analysis####
+####Analysis - Community####
+
+dat = COMMarray[2:ncol(COMMarray)]
+Blob_sp = COMMarray$growth_form
+Blob_microsite = COMMarray$Habitat
+
+PCA <- ade4::dudi.pca(dat, center = T, scale = T, scannf = F, nf = ncol(dat))
+means<- PCA$cent
+sds <- PCA$norm
+eigen <- round(PCA$eig / sum(PCA$eig) * 100, 1)
+
+signx <- ifelse(sign(sum(sign(PCA$c1[, "CS1"]))) < 0, -1 ,1)
+signy <- ifelse(sign(sum(sign(PCA$c1[, "CS2"]))) < 0, -1, 1)
+x = signx * PCA$li[, 'Axis1']
+y = signy * PCA$li[, 'Axis2']
+Axis1 <- signx * PCA$li[, 'Axis1']
+Axis2 <- signy * PCA$li[, 'Axis2']
+
+mult <- 4
+arrows_all = tibble(
+  Trait = titre, 
+  xend = signx * PCA$co[, 'Comp1'] * mult,
+  yend = signy * PCA$co[, 'Comp2'] * mult,
+  x = rep(0, length(titre)),
+  y = rep(0, length(titre)))
+
+####Plot####
+
+plot_data_all = tibble(Axis1 = x, 
+                       Axis2 = y,
+                       #Species = Blob_sp,
+                       Microsite = Blob_microsite)
+
+ggplot(plot_data_all,
+       aes(x = Axis1,
+           y = Axis2)) +
+  scale_alpha(range = c(0.3, 0.7)) +
+  geom_point(size = 0.2,
+             alpha = 0.6,
+             colour = "grey50") +
+  stat_density_2d(geom = "polygon",
+                  aes(fill = Microsite,
+                      alpha = ..nlevel..),
+                  contour_var = "ndensity",
+                  breaks = c(0.5, 0.9)) +
+  stat_density_2d(geom = "polygon",
+                  aes(colour = Microsite),
+                  contour_var = "ndensity",
+                  fill = NA,
+                  breaks = c(0.5)) +
+  guides(alpha = FALSE,
+         fill = NULL) +
+  scale_fill_manual(values = c('goldenrod1', 'forestgreen')) +
+  scale_colour_manual(values = c('goldenrod1', 'forestgreen')) +
+  theme_classic() +
+  theme(legend.position = 'bottom',
+        plot.title = element_text(size = 20)) +
+  xlim(-1.5,1.5) +
+  ylim(-1.5,1.5) +
+  labs(x = "PC1",
+       y = "PC2") +
+  theme(legend.position = 'bottom')
+
+ggsave("figures/community_pca.png",
+       width = 11,
+       height = 7)
+
+
+####Analysis - Functional traits####
+
+dat = FTarray[4:ncol(FTarray)]
+Blob_sp = FTarray$Species
+Blob_microsite = FTarray$Habitat
 
 PCA <- ade4::dudi.pca(dat, center = T, scale = T, scannf = F, nf = ncol(dat))
 means<- PCA$cent
