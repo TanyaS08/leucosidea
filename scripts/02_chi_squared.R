0b12d8be6c7e19c20f16b523d03082fa888edcc7
Tanya Strydom <tanya.strydom@icloud.com>
2024-08-15 13:02:35 +0100

    :sparkles: write chisq results

--- scripts/02_chi_squared.R
+++ scripts/02_chi_squared.R
@@ -3,7 +3,8 @@
 
 ####Import data####
 spp <- read.table("data/spp_richness.txt",
-                    header = T) %>%
+                    header = T) %>% 
+  select(-c(Dead, Seedling)) %>%
   # set abundance to 1 if cover > 0
   mutate(across(Ajuga_ophrydis:Tristachya_leucothrix, 
                 ~ ifelse(.x == 0, 0 ,1))) %>%
@@ -21,5 +22,35 @@ spp <- spp[,-1][colSums(spp[,-1]) > 26] %>%
 
 ####Chi2 test####
 
-chi <- chisq.test(spp,
-                  correct = F)
+ChiSq <- t(spp)
+ChiSq <- as.data.frame(ChiSq)
+ChiSq$Control_ab <- 135 - ChiSq$away
+ChiSq$Under_ab <- 135 - ChiSq$under
+tempabun <- which(ChiSq$away + ChiSq$under > 26.5)
+length(tempabun)
+dat <- ChiSq[tempabun,]
+
+#Empty Matrix to store results
+Results <- matrix(data = NA, ncol = 4, nrow = dim(dat)[1])
+colnames(Results) <- c("Spp", "Chi2", "df", "p")
+
+#loop for chi test
+for (i in 1: dim(dat)[1]) {
+  temp.tab <- t(structure(array(as.numeric(dat[i, c(1:4)])), dim = c(2, 2)))
+  colnames(temp.tab) <- c("Control", "Under")
+  rownames(temp.tab) <- c("Present", "Absent")
+  temp.res <- chisq.test(temp.tab) #, simulate.p.value = TRUE)
+  Results[i, 1] <- as.character(row.names(dat)[i])
+  Results[i, 2] <- round(temp.res$statistic, 4)
+  Results[i, 3] <- as.numeric(temp.res$parameter)
+  Results[i, 4] <- round(temp.res$p.value, 4)
+}
+
+Results
+
+Results <- as.data.frame(Results)
+Results$Control <- dat$Control
+Results$Under <- dat$Under
+Results <- Results[c(1,5,6,2,3,4)]
+
+write.csv(Results, "outputs/chisq_results.csv")
