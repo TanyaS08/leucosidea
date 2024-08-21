03f380a4decef3092f27ed600203671b952984fe
Tanya Strydom <tanya.strydom@icloud.com>
2024-08-21 10:47:38 +0200

    :racehorse: NMDS for traits

--- scripts/03_nmds.R
+++ scripts/03_nmds.R
@@ -109,32 +109,17 @@ write.csv(permanova_all,
 
 ####Analysis - Functional traits####
 
-dat = FTarray[4:ncol(FTarray)]
-Blob_sp = FTarray$Species
-Blob_microsite = FTarray$Habitat
+ft_mds <- metaMDS(FTarray[4:ncol(FTarray)], distance = "bray")
 
-PCA <- ade4::dudi.pca(dat, center = T, scale = T, scannf = F, nf = ncol(dat))
-means<- PCA$cent
-sds <- PCA$norm
-eigen <- round(PCA$eig / sum(PCA$eig) * 100, 1)
+####Plot####
 
-signx <- ifelse(sign(sum(sign(PCA$c1[, "CS1"]))) < 0, -1 ,1)
-signy <- ifelse(sign(sum(sign(PCA$c1[, "CS2"]))) < 0, -1, 1)
-x = signx * PCA$li[, 'Axis1']
-y = signy * PCA$li[, 'Axis2']
-Axis1 <- signx * PCA$li[, 'Axis1']
-Axis2 <- signy * PCA$li[, 'Axis2']
+ft_traits_nmds <- as.data.frame(ft_mds$species) %>% 
+  mutate(traits = row.names(.))
 
-titre <- c(Chlorophyll = "Chlorophyll~(mg/m^2)", PHeight = "Plant~height~(m)", SLA = "SLA~(cm^2%.%g)",
-           LDMC = "LDMC", Toughness = "Toughness~(N)")[colnames(dat)]
-
-mult <- 4
-arrows_all = tibble(
-  Trait = titre, 
-  xend = signx * PCA$co[, 'Comp1'] * mult,
-  yend = signy * PCA$co[, 'Comp2'] * mult,
-  x = rep(0, length(titre)),
-  y = rep(0, length(titre)))
+ft_species_nmds <- as.data.frame(ft_mds$points) %>%
+  mutate(Microsite = FTarray$Habitat,
+         Site = str_extract(row.names(.), "^.{1}"),
+         species = FTarray$Species)
 
 ft_spp_colours = tibble(
   species = c("Commelina", "Helichrysum","Miscanthus", "Oxalis",
@@ -145,60 +130,38 @@ ft_spp_colours = tibble(
              "Forb", "Grass", "Grass")
 )
 
-plot_data_all = tibble(Axis1 = x, 
-                       Axis2 = y,
-                       Species = Blob_sp,
-                       Microsite = Blob_microsite,
-                       fgroup = ifelse(Blob_sp %in% c("Miscanthus", "Themeda", "Tristachya"),
-                                       "Grass",
-                                       "Forb"))
-
 ####Plot####
 
-ggplot(plot_data_all,
-         aes(x = Axis1,
-             y = Axis2)) +
+ggplot(ft_species_nmds,
+       aes(x = MDS1,
+           y = MDS2)) +
   scale_alpha(range = c(0.3, 0.7)) +
-  geom_point(size = 0.2,
+  geom_point(size = 0.8,
              alpha = 0.6,
              colour = "grey50") +
   stat_density_2d(geom = "polygon",
-                  aes(fill = Species,
-                      alpha = ..nlevel..),
+                  aes(fill = species,
+                      alpha = after_stat(nlevel)),
                   contour_var = "ndensity",
-                  breaks = c(0.5, 0.95))+
+                  breaks = c(0.5, 0.9)) +
   stat_density_2d(geom = "polygon",
-                  aes(colour = Species),
+                  aes(colour = species),
                   contour_var = "ndensity",
                   fill = NA,
                   breaks = c(0.5)) +
   guides(alpha = FALSE,
          fill = NULL) +
   facet_grid(cols = vars(Microsite)) +
-  geom_segment(data = arrows_all,
-               aes(xend = xend, 
-                   yend = yend,
-                   x = x,
-                   y = y),
-               arrow = arrow(length=unit(0.2,"cm")),
-               size = 0.3) +
-  geom_label_repel(data = arrows_all,
-                   aes(label = Trait,
-                       x = xend,
-                       y = yend),
-                   hjust = 0.1,
-                   parse = TRUE, 
-                   size = 3,
-                   check_overlap = FALSE) +
   scale_fill_manual(values = ft_spp_colours$colour,
                     aesthetics = c("colour", "fill")) +
   theme_classic() +
   theme(legend.position = 'bottom',
         plot.title = element_text(size = 20)) +
-  xlim(-4.3,4.3) +
-  ylim(-4.3,4.3) +
-  labs(x = "PC1",
-       y = "PC2")
+  xlim(-0.4,0.4) +
+  ylim(-0.4,0.4) +
+  labs(x = "MDS1",
+       y = "MDS2",
+       caption = paste0( "Stress = ", round(ft_mds$stress*100, digits = 2), "%"))
 
 ggsave("figures/FT_pca.png",
        width = 11,
